2b2ada6329e35f96804d3c33bb2a33f98d26e848
Carlos Vinicios De Souza Azeredo <carlosvinicios@id.uff.br>
2024-06-24 21:39:53 -0300

    config: configuração inicial da api

---
+++ back-end/api-gerenciador-de-tarefas/src/main/java/carpelune/ApiGerenciadorDeTarefasApplication.java
@@ -0,0 +1,13 @@
+package carpelune;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiGerenciadorDeTarefasApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApiGerenciadorDeTarefasApplication.class, args);
+	}
+
+}
