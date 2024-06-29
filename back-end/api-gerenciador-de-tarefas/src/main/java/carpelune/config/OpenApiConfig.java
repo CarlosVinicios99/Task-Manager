d3dabb163b6a24630e4d55ace6f9b96174e2ac23
Carlos Vinicios De Souza Azeredo <carlosvinicios@id.uff.br>
2024-06-28 21:24:15 -0300

    config: configuração do swagger

---
+++ back-end/api-gerenciador-de-tarefas/src/main/java/carpelune/config/OpenApiConfig.java
@@ -0,0 +1,24 @@
+package carpelune.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.Info;
+import io.swagger.v3.oas.models.info.License;
+
+
+@Configuration
+public class OpenApiConfig {
+		
+	@Bean
+	public OpenAPI customOpenApi() {
+		return new OpenAPI()
+			.info(new Info()
+			.title("API Task Manager")
+			.version("v1")
+			.description("API desenvolvida para prever serviços de controle e gerenciamento de tarefas")
+			.license(new License().name("Apache 2.0").url("https://github")));
+	}	
+	
+}
