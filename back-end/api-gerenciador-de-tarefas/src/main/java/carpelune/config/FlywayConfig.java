2f1abd99a06198fe87dcdfa2b521f58ec11cff90
Carlos Vinicios De Souza Azeredo <carlosvinicios@id.uff.br>
2024-06-28 21:19:00 -0300

    config: configuração do flyway

---
+++ back-end/api-gerenciador-de-tarefas/src/main/java/carpelune/config/FlywayConfig.java
@@ -0,0 +1,17 @@
+package carpelune.config;
+
+
+import org.springframework.boot.autoconfigure.flyway.FlywayMigrationStrategy;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class FlywayConfig {
+	
+	 public FlywayMigrationStrategy flywayMigrationStrategy() {
+	        return flyway -> {
+	            flyway.repair();
+	            flyway.migrate();
+	        };
+	    }
+}
+
