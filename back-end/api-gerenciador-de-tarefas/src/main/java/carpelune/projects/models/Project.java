6b8d57a0f43fa250804e9d59b87eba7725464286
Carlos Vinicios De Souza Azeredo <carlosvinicios@id.uff.br>
2024-06-30 00:39:50 -0300

    feature: criação da classe de Projeto

--- back-end/api-gerenciador-de-tarefas/src/main/java/carpelune/projects/models/Project.java
+++ back-end/api-gerenciador-de-tarefas/src/main/java/carpelune/projects/models/Project.java
@@ -1,5 +1,105 @@
 package carpelune.projects.models;
 
-public class Project {
+import java.io.Serializable;
+import java.util.Objects;
+import java.util.UUID;
 
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+
+@Entity
+@Table(name = "projects")
+public class Project implements Serializable {
+	
+	private static final long serialVersionUID = 1L;
+	
+	@Id
+	@GeneratedValue
+	private UUID id;
+	
+	@Column()
+	private String title;
+	
+	@Column()
+	private String description;
+	
+	@Column()
+	private Long creationTimestamp;
+	
+	@Column(name = "workspace_id")
+	private UUID workspaceId;
+	
+	
+	public Project() {
+		
+	}
+	
+	public Project(UUID id, String title, String description, Long creationTimestamp, String workspaceId) {
+		this.id = id;
+		this.title = title;
+		this.description = description;
+		this.creationTimestamp = creationTimestamp;
+	}
+
+	public UUID getId() {
+		return id;
+	}
+
+	public void setId(UUID id) {
+		this.id = id;
+	}
+
+	public String getTitle() {
+		return title;
+	}
+
+	public void setTitle(String title) {
+		this.title = title;
+	}
+
+	public String getDescription() {
+		return description;
+	}
+
+	public void setDescription(String description) {
+		this.description = description;
+	}
+
+	public Long getCreationTimestamp() {
+		return creationTimestamp;
+	}
+
+	public void setCreationTimestamp(Long creationTimestamp) {
+		this.creationTimestamp = creationTimestamp;
+	}
+
+	public UUID getWorkspaceId() {
+		return workspaceId;
+	}
+
+	public void setWorkspaceId(UUID workspaceId) {
+		this.workspaceId = workspaceId;
+	}
+
+	@Override
+	public int hashCode() {
+		return Objects.hash(creationTimestamp, description, id, title, workspaceId);
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Project other = (Project) obj;
+		return Objects.equals(creationTimestamp, other.creationTimestamp)
+			&& Objects.equals(description, other.description) && Objects.equals(id, other.id)
+			&& Objects.equals(title, other.title) && Objects.equals(workspaceId, other.workspaceId);
+	}
 }
