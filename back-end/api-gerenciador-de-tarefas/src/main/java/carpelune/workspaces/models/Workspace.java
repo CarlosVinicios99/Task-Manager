bb268f0c6faf5e1e11d07a5a49916a118d1b8291
Carlos Vinicios De Souza Azeredo <carlosvinicios@id.uff.br>
2024-06-29 23:49:26 -0300

    feature: criação da classe de workspaces

--- back-end/api-gerenciador-de-tarefas/src/main/java/carpelune/workspaces/models/Workspace.java
+++ back-end/api-gerenciador-de-tarefas/src/main/java/carpelune/workspaces/models/Workspace.java
@@ -1,5 +1,98 @@
 package carpelune.workspaces.models;
 
-public class Workspace {
+import java.io.Serializable;
+import java.util.Objects;
+import java.util.UUID;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+
+@Entity
+@Table(name = "workspaces")
+public class Workspace implements Serializable {
+	
+	private static final long serialVersionUID = 1L;
+	
+	@Id
+	@GeneratedValue
+	private UUID id;
+	
+	@Column()
+	private String title;
+	
+	@Column()
+	private String description;
+	
+	@GeneratedValue
+	@Column(name = "creation_timestamp")
+	private Long creationTimestamp;
+	
+	
+	public Workspace() {
+		
+	}
+	
+	public Workspace(UUID id, String title, String description, Long creationTimestamp) {
+		this.id = id;
+		this.title = title;
+		this.description = description;
+		this.creationTimestamp = creationTimestamp;
+	}
+	
+
+	public UUID getId() {
+		return id;
+	}
+
+	public void setId(UUID id) {
+		this.id = id;
+	}
+
+	public String getTitle() {
+		return title;
+	}
+
+	public void setTitle(String title) {
+		this.title = title;
+	}
+
+	public String getDescription() {
+		return description;
+	}
+
+	public void setDescription(String description) {
+		this.description = description;
+	}
+
+	public Long getCreationTimestamp() {
+		return creationTimestamp;
+	}
+
+	public void setCreationTimestamp(Long creationTimestamp) {
+		this.creationTimestamp = creationTimestamp;
+	}
+	
+	
+	@Override
+	public int hashCode() {
+		return Objects.hash(creationTimestamp, description, id, title);
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Workspace other = (Workspace) obj;
+		return Objects.equals(creationTimestamp, other.creationTimestamp)
+				&& Objects.equals(description, other.description) && Objects.equals(id, other.id)
+				&& Objects.equals(title, other.title);
+	}
 
 }
