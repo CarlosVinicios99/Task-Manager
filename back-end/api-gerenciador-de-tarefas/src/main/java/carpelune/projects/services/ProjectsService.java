98f26dbe5b95bee3c78ecec042d23e1e7e490e64
Carlos Vinicios De Souza Azeredo <carlosvinicios@id.uff.br>
2024-07-09 20:18:23 -0300

    feature: criação de serviço para cadastrar um novo projeto

--- back-end/api-gerenciador-de-tarefas/src/main/java/carpelune/projects/services/ProjectsService.java
+++ back-end/api-gerenciador-de-tarefas/src/main/java/carpelune/projects/services/ProjectsService.java
@@ -1,5 +1,55 @@
 package carpelune.projects.services;
 
-public class ProjectsService {
+import java.time.Instant;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Service;
+
+import carpelune.projects.dto.CreateProjectDTO;
+import carpelune.projects.models.Project;
+import carpelune.projects.repositories.ProjectsRepository;
+
+@Service
+public class ProjectsService {
+	
+	private Logger logger = Logger.getLogger(ProjectsService.class.getName());
+	
+	@Autowired
+	private ProjectsRepository projectsRepository;
+	
+	
+	public ResponseEntity<Project> createProject(CreateProjectDTO createProjectDTO){
+		
+		this.logger.log(Level.INFO, "creating a new project");
+		if(createProjectDTO.workspaceId() != null || createProjectDTO.workspaceId().toString().isEmpty()) {
+			this.logger.log(Level.WARNING, "the provided workspace ID is undefined");
+			return ResponseEntity.status(HttpStatus.UNPROCESSABLE_ENTITY).build();
+		}
+		
+		try {
+			Project newProject = new Project();
+			newProject.setTitle(createProjectDTO.title());
+			newProject.setDescription(createProjectDTO.description());
+			newProject.setWorkspaceId(createProjectDTO.workspaceId());
+			newProject.setCreationTimestamp(Instant.now().toEpochMilli());
+			
+			this.logger.log(Level.WARNING, "saving the new project in the database");
+			newProject = this.projectsRepository.save(newProject);
+			
+			return ResponseEntity.status(HttpStatus.OK).body(newProject);
+		}
+		catch(Exception error) {
+			this.logger.log(Level.SEVERE, "error while registering project");
+			return ResponseEntity.status(HttpStatus.SERVICE_UNAVAILABLE).build();
+		}
+	}
+	
+	//TODO public ResponseEntity<FindProjectDTO> findProjectById()
+	//TODO public ResponseEntity<UpdateProjectDTO> updateProject()
+	//TODO public ResponseEntity<Void> deleteProject()
+	
 }
