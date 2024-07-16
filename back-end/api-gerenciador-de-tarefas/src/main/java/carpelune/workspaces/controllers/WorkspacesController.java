ee8dcb6b2da8d43f3c956a04184b97f48ddb51a8
Carlos Vinicios De Souza Azeredo <carlosvinicios@id.uff.br>
2024-07-15 23:21:53 -0300

    feature: criação da camada de controle para o módulo de workspace

--- back-end/api-gerenciador-de-tarefas/src/main/java/carpelune/workspaces/controllers/WorkspacesController.java
+++ back-end/api-gerenciador-de-tarefas/src/main/java/carpelune/workspaces/controllers/WorkspacesController.java
@@ -1,5 +1,49 @@
 package carpelune.workspaces.controllers;
 
-public class WorkspacesController {
+import java.util.UUID;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import carpelune.workspaces.dto.CreateWorkspaceDTO;
+import carpelune.workspaces.dto.DeleteWorkspaceDTO;
+import carpelune.workspaces.dto.UpdateWorkspaceDTO;
+import carpelune.workspaces.models.Workspace;
+import carpelune.workspaces.services.WorkspacesService;
+
+@CrossOrigin
+@RestController
+@RequestMapping("/workspaces")
+public class WorkspacesController {
+	
+	@Autowired
+	private WorkspacesService workspacesService;
+	
+	
+	@PostMapping()
+	public ResponseEntity<Workspace> createWorkspace(CreateWorkspaceDTO createWorkspaceDTO){
+		return this.workspacesService.createWorkspace(createWorkspaceDTO);
+	}
+	
+	@GetMapping("/{workspaceId}")
+	public ResponseEntity<Workspace> findWorkspaceById(UUID workspaceId){
+		return this.workspacesService.findWorkspaceById(workspaceId);
+	}
+	
+	@PutMapping()
+	public ResponseEntity<Void> updateWorkspace(UpdateWorkspaceDTO updateWorkspaceDTO){
+		return this.workspacesService.updateWorkspace(updateWorkspaceDTO);
+	}
+	
+	@DeleteMapping()
+	public ResponseEntity<Void> deleteWorkspace(DeleteWorkspaceDTO deleteWorkspaceDTO){
+		return this.workspacesService.deleteWorkspace(deleteWorkspaceDTO);
+	}
 }
