92616af4f85ff9773357a2601e053f49366cb7b3
Carlos Vinicios De Souza Azeredo <carlosvinicios@id.uff.br>
2025-03-04 15:07:33 -0300

    feature: criação de estilos para o componente da seção de 'Bem-vindo de volta

--- front-end/gerenciador-de-tarefas-web/src/components/welcome/WelcomeBackSection.tsx
+++ front-end/gerenciador-de-tarefas-web/src/components/welcome/WelcomeBackSection.tsx
@@ -1,12 +1,14 @@
-import React from 'react'
+import "./WelcomeBackSection.css"
 
 const WelcomeBackSection = () => {
   return (
     <div className='welcome-back-content'>
         <h1>Welcome Back!</h1>
-        <p>To keep connected with us</p>
-        <p>please login with your personal info</p>
-        <button>SIGN IN</button>
+        <div className="text-content">
+          <p>To keep connected with us</p>
+          <p>please login with your personal info</p>
+        </div>
+        <button className="signin-button">SIGN IN</button>
     </div>
   )
 }
--- front-end/gerenciador-de-tarefas-web/src/pages/welcome/Welcome.tsx
+++ front-end/gerenciador-de-tarefas-web/src/pages/welcome/Welcome.tsx
@@ -1,9 +1,11 @@
+import WelcomeBackSection from '../../components/welcome/WelcomeBackSection';
 import './Welcome.css';
 
 const Welcome = () => {
 
   return (
     <div className="welcome-content">
+      <WelcomeBackSection/>
     </div>
   )
 
