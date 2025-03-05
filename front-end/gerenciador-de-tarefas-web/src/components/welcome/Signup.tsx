0f2f8a34b4fcdd8905e16a9ba4a4e24ee38c9a32
Carlos Vinicios De Souza Azeredo <carlosvinicios@id.uff.br>
2025-03-04 22:28:09 -0300

    feature: criação de componente de input para formulários

--- front-end/gerenciador-de-tarefas-web/src/components/welcome/Signup.tsx
+++ front-end/gerenciador-de-tarefas-web/src/components/welcome/Signup.tsx
@@ -1,9 +1,13 @@
+import InputForms from "../ui/InputForms"
+
 const Signup = () => {
   return (
     <div className="signup-content">
         <h1>Create Your Account</h1>
         <form>
-            
+            <InputForms key={1} src="" placeholder="Name" type="text"/>
+            <InputForms key={2} src="" placeholder="E-mail" type="text"/>
+            <InputForms key={3} src="" placeholder="Password" type="password"/>
         </form>
     </div>
   )
