20a051aa361d92b5cdf2c495e99a494386bd084a
Carlos Vinicios De Souza Azeredo <carlosvinicios@id.uff.br>
2025-03-04 23:13:23 -0300

    feature: estilização inicial da tela de boas vindas

--- front-end/gerenciador-de-tarefas-web/src/components/welcome/Signup.tsx
+++ front-end/gerenciador-de-tarefas-web/src/components/welcome/Signup.tsx
@@ -1,15 +1,17 @@
 import InputForms from "../ui/InputForms"
 
+import "./Signup.css"
+
 const Signup = () => {
   return (
     <div className="signup-content">
-        <h1>Create Your Account</h1>
-        <form>
+        <h1 className="title">Create Your Account</h1>
+        <form className="form-content">
             <InputForms key={1} src={import.meta.env.VITE_ICON_NAME} placeholder="Name" type="text"/>
             <InputForms key={2} src={import.meta.env.VITE_ICON_EMAIL} placeholder="E-mail" type="text"/>
             <InputForms key={3} src={import.meta.env.VITE_ICON_PASSWORD} placeholder="Password" type="password"/>
         </form>
-        <button>Signup</button>
+        <button className="button-signup">SIGN UP</button>
     </div>
   )
 }
