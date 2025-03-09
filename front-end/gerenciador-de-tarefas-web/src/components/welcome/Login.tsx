205d900d4375ed80e0f8c710727dd8d68fde71a1
Carlos Vinicios De Souza Azeredo <carlosvinicios@id.uff.br>
2025-03-09 14:38:38 -0300

    feature: criação dos elementos do componente de Login

--- front-end/gerenciador-de-tarefas-web/src/components/welcome/Login.tsx
+++ front-end/gerenciador-de-tarefas-web/src/components/welcome/Login.tsx
@@ -1,7 +1,17 @@
+import InputForms from "../ui/InputForms"
+
 const Login = () => {
+  
   return (
-    <div>Login</div>
+    <div className="login-content">
+        <h1>Login</h1>
+        <p>Enter your credentials</p>
+        <InputForms key={4} src={import.meta.env.VITE_ICON_EMAIL} placeholder="E-mail" type="text"/>
+        <InputForms key={5} src={import.meta.env.VITE_ICON_PASSWORD} placeholder="Password" type="password"/>
+        <button>Login</button>
+    </div>
   )
+
 }
 
 export default Login
