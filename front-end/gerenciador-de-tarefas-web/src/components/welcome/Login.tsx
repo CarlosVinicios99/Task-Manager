8ce2bb3a7143a5f8fd37fd6b23ecdd3e78929dbe
Carlos Vinicios De Souza Azeredo <carlosvinicios@id.uff.br>
2025-03-09 15:01:57 -0300

    feature: criando estilos do componente de login

--- front-end/gerenciador-de-tarefas-web/src/components/welcome/Login.tsx
+++ front-end/gerenciador-de-tarefas-web/src/components/welcome/Login.tsx
@@ -1,14 +1,17 @@
 import InputForms from "../ui/InputForms"
 
+import "./Login.css"
+
 const Login = () => {
   
   return (
     <div className="login-content">
-        <h1>Login</h1>
-        <p>Enter your credentials</p>
-        <InputForms key={4} src={import.meta.env.VITE_ICON_EMAIL} placeholder="E-mail" type="text"/>
-        <InputForms key={5} src={import.meta.env.VITE_ICON_PASSWORD} placeholder="Password" type="password"/>
-        <button>Login</button>
+        <h1 className="title">Login</h1>
+        <form className="form-login-content">
+            <InputForms key={4} src={import.meta.env.VITE_ICON_EMAIL} placeholder="E-mail" type="text"/>
+            <InputForms key={5} src={import.meta.env.VITE_ICON_PASSWORD} placeholder="Password" type="password"/>
+        </form>
+        <button className="button-login">LOGIN</button>
     </div>
   )
 
