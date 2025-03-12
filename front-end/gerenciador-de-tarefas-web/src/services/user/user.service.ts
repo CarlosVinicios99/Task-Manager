fbb8744ee96f34605912164bda750bd4186dcdd8
Carlos Vinicios De Souza Azeredo <carlosvinicios@id.uff.br>
2025-03-12 20:25:07 -0300

    feature: criação de integração com o serviço de criação de usuário no front

--- front-end/gerenciador-de-tarefas-web/src/services/user/user.service.ts
+++ front-end/gerenciador-de-tarefas-web/src/services/user/user.service.ts
@@ -1,3 +1,6 @@
+import { User } from "../../interfaces/User";
+import { CreateUserDto } from "./interfaces/CreateUserDto";
+
 export class UserService {
 
     private static instance: UserService
@@ -9,6 +12,26 @@ export class UserService {
             UserService.instance = new UserService();
         }
         return UserService.instance
+    }
+
+    async createUser(data: CreateUserDto){
+        try{
+            const url: string = `${import.meta.env.TASK_MANAGER_API_URL}user`
+            const response = await fetch(url, {
+                method: "POST",
+                headers: {
+                    "Content-Type": "application/json"
+                },
+                body: JSON.stringify(data),
+            })
+
+            const user: User = await response.json()
+
+            return user
+        }
+        catch(error){
+            return null
+        }
     }
 
 }
